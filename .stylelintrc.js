c99b43df0b900fb988dc807370173bb593a329c5
Joseph Fusco <jfusco@masonite.com>
2019-09-30 11:29:50 -0400

    stylelint order

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,19 @@
 module.exports = {
-  extends: "@masonite/stylelint-config",
+  extends: '@masonite/stylelint-config',
+  plugins: [
+    'stylelint-order',
+    'stylelint-config-rational-order/plugin', // remove to keep only alpha sort
+  ],
   rules: {
-    "scss/dollar-variable-colon-space-after": "at-least-one-space",
-    "max-empty-lines": 2
-  }
+    'scss/dollar-variable-colon-space-after': 'at-least-one-space',
+    'max-empty-lines': 2,
+    'order/properties-order': [],
+    'plugin/rational-order': [
+      true,
+      {
+        'border-in-box-model': false,
+        'empty-line-between-groups': false,
+      },
+    ],
+  },
 };
