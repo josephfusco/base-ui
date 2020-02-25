71ba616d0429256e5f8a62d8e115fbb657e6dcaa
Joseph Fusco <jfusco@masonite.com>
2020-02-25 12:04:25 -0500

    Support backgrounds addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,26 @@
+import { addParameters } from '@storybook/html';
+
 // Import base-ui.scss into all stories.
 import '!style-loader!css-loader!sass-loader!./../src/scss/base-ui.scss';
 import '!style-loader!css-loader!sass-loader!./preview.scss';
+
+addParameters({
+  backgrounds: [
+    { name: 'twitter', value: '#00aced', default: true },
+    { name: 'facebook', value: '#3b5998' },
+  ],
+});
+
+addParameters({
+  backgrounds: [
+    {
+      name: 'gray',
+      value: '#F3F3F3',
+      default: true,
+    },
+    {
+      name: 'white',
+      value: '#FFFFFF',
+    },
+  ],
+});
